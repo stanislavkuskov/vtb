06ba2ed2383a0005fade5ad50f6055dc9e2674f7
kuskov_stanislav <kuskov.stanislav@gmail.com>
2017-07-03 17:24:03 +0300

    kuskov_test. First telegramm robot creation from kuskov. Test with name sending "hello username"

--- kuskov_test/main.py
+++ kuskov_test/main.py
@@ -1,4 +1,21 @@
+#vtbRObot
+
+import telebot
+TOKEN='435200176:AAFWWdzzmP6FtRY5d32OJF4yeCjq-tRbBP4'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+
+def hello(message):
+    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
+
 def main():
+    bot.polling()
+
+
 
     pass
 if __name__ == '__main__':
